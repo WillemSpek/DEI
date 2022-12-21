d7fde49cb8edb9ed871e2f77d06fee7be18c9095
ist1105558 <ist1105558@tecnico.ulisboa.pt>
2022-12-21 18:51:39 +0000

    progress Q1.c

--- hw1-q1.py
+++ hw1-q1.py
@@ -18,10 +18,19 @@ def configure_seed(seed):
     np.random.seed(seed)
 
 
-def softmax(x, axis=None):
-    x = x - x.max(axis=axis, keepdims=True)
-    y = np.exp(x)
-    return y / y.sum(axis=axis, keepdims=True)
+def softmax(x, axis=0):
+    y = x - np.max(x, axis=axis)
+    z = np.exp(y)
+    return z / z.sum(axis=axis, keepdims=True)
+
+
+def dsoftmax(X):
+    derivs = []
+    for i in range(X.shape[1]):
+        x = X[:,i]
+        I = np.eye(x.shape[0])
+        derivs.append(softmax(x) * (I - softmax(x).T))
+    return np.array(derivs)
 
 
 def ReLU(x_i):
@@ -29,6 +38,10 @@ def ReLU(x_i):
     x_i (n_features): np array for a single training sample
     """
     return x_i * (x_i > 0)
+
+
+def dReLU(x_i):
+    return 1 * (x_i > 0)
 
 
 class LinearModel(object):
@@ -62,9 +75,6 @@ class LinearModel(object):
 class Perceptron(LinearModel):
     def __init__(self, n_classes, n_features):
         super().__init__(n_classes, n_features)
-
-    def _sign(x_i):
-        return 1 * (x_i > 0)
 
     def update_weight(self, x_i, y_i, **kwargs):
         """
@@ -115,15 +125,58 @@ class MLP(object):
     # Q3.2b. This MLP skeleton code allows the MLP to be used in place of the
     # linear models with no changes to the training loop or evaluation code
     # in main().
-    def __init__(self, n_classes, n_features, hidden_size):
-        # Initialize an MLP with a single hidden layer.
-        raise NotImplementedError
+    def __init__(self, n_classes, n_features, hidden_size, n_layers):
+        # Constants
+        self.n_classes = n_classes
+        self.n_layers = 3
 
-    def predict(self, X):
+        # Iterables
+        self.W = np.array([.1 * np.ones((hidden_size, n_features)), 
+                           .1 * np.ones((hidden_size, hidden_size)),
+                           .1 * np.ones((n_classes, hidden_size))])
+        self.biases = np.array([.1 * np.ones(hidden_size),
+                                .1 * np.ones(hidden_size), 
+                                .1 * np.ones(n_classes)])          
+        self.activate = np.array([ReLU, ReLU, softmax])
+
+    def _one_hot(self, y):
+        return np.eye(self.n_classes)[y].T
+
+    def _forward(self, X):
         # Compute the forward pass of the network. At prediction time, there is
         # no need to save the values of hidden nodes, whereas this is required
         # at training time.
-        raise NotImplementedError
+        scores = []
+        score = X.T
+        for i in range(self.n_layers):
+            score = np.dot(self.W[i], score) + np.tile(self.biases[i], (score.shape[1], 1)).T
+            score = self.activate[i](score)
+            scores.append(score)
+        return scores[-1], scores[:-1]
+
+    def predict(self, X):
+        score = X.T
+        for i in range(self.n_layers):
+            score = np.dot(self.W[i], score) + np.tile(self.biases[i], (score.shape[1], 1)).T
+            score = self.activate[i](score)
+        return np.argmax(score, axis=0)
+        
+    def _backprop(self, X, y_pred, y, hiddens):
+        grad_weights = []
+        grad_biases = []
+
+        grad_z = y_pred - y
+        for i in range(self.n_layers - 1, -1, -1):
+            h = X.T if i == 0 else hiddens[i - 1]
+            grad_weights.append(grad_z.dot(h.T))
+            grad_biases.append(grad_z.sum(axis=1, keepdims=False))
+
+            grad_h = self.W[i].T.dot(grad_z)
+            grad_z = grad_h * dReLU(h)
+
+        grad_weights.reverse()
+        grad_biases.reverse()
+        return grad_weights, grad_biases
 
     def evaluate(self, X, y):
         """
@@ -137,7 +190,13 @@ class MLP(object):
         return n_correct / n_possible
 
     def train_epoch(self, X, y, learning_rate=0.001):
-        raise NotImplementedError
+        y = self._one_hot(y)
+        y_pred, hiddens = self._forward(X)
+        grads, grad_biases = self._backprop(X, y_pred, y, hiddens)
+
+        for i in range(len((self.W))):
+            self.W[i] -= learning_rate * grads[i]
+            self.biases[i] -= learning_rate * grad_biases[i]
 
 
 def plot(epochs, valid_accs, test_accs):
@@ -147,7 +206,8 @@ def plot(epochs, valid_accs, test_accs):
     plt.plot(epochs, valid_accs, label='validation')
     plt.plot(epochs, test_accs, label='test')
     plt.legend()
-    plt.savefig('Perceptron_loss.png')
+    plt.title('Accuracy for the logistic regression model per epoch')
+    plt.savefig('Logistic_acc.png')
 
 
 def main():
@@ -209,3 +269,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def backpropagation(self,y, z_s, a_s):
+  dw = []  # dC/dW
+  db = []  # dC/dB
+  deltas = [None] * len(self.weights)  # delta = dC/dZ  known as error for each layer
+  # insert the last layer error
+  deltas[-1] = ((y-a_s[-1])*(self.getDerivitiveActivationFunction(self.activations[-1]))(z_s[-1]))
+  # Perform BackPropagation
+  for i in reversed(range(len(deltas)-1)):
+    deltas[i] = self.weights[i+1].T.dot(deltas[i+1])*(self.getDerivitiveActivationFunction(self.activations[i])(z_s[i]))        
+    batch_size = y.shape[1]
+    db = [d.dot(np.ones((batch_size,1)))/float(batch_size) for d in deltas]
+    dw = [d.dot(a_s[i].T)/float(batch_size) for i,d in enumerate(deltas)]
+    # return the derivitives respect to weight matrix and biases
+    return dw, db
